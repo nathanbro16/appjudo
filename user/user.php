c0f308ac573c3d081482c843c51ec90e1bdbacba
nathanbrohan@gmail.com <nathanbrohan@gmail.com>
2019-04-06 14:35:41 +0200

    replace user grade to judoka grade

--- user/user.php
+++ user/user.php
@@ -15,9 +15,8 @@ require 'header.php';
 	<div style="padding-bottom:2%; ">
 		<div class="card" >
 			<div class="card-body row">
-				<h5 class="col" style="margin-bottom:0;"><span id="bonjour">Bonjour</span>, <?= $userinfo->getname();?> <?= $userinfo->getsurname();?>. Il est <span class="badge badge-secondary" id='heure' ></span></h5>
-				<h5 class="col"></h5>
-				<h5 class="col text-right" style="margin-bottom:0;"> <img src="../grade/<?= $jugrd['html']?>.png" width="60" height="30" class="d-inline-block align-top" alt=""> / <?= $jugrd['name']?> / <?=$user->getnameyear()?></h5>
+				<h5 class="col-sm-6 col-md-6 col-lg-6" style="margin-bottom:0;"><span id="bonjour">Bonjour</span>, <?= $userinfo->getname();?> <?= $userinfo->getsurname();?>. Il est <span class="badge badge-secondary" id='heure' ></span></h5>
+				<h5 class="col text-right" style="margin-bottom:0;"> <img src="../grade/<?= $jugrd['html']?>.png" width="60" height="30" class="d-inline-block align-top" alt=""> / X (nom grade)/ X (nom catégorie)</h5>
 			</div>
 		</div>
 			<script type="text/javascript">window.onload = bonjs(); heure('heure');</script>
